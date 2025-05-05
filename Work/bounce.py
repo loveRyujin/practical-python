a7831758ecc1124fe6c5cd0ae40be4cb4015111e
lichen <1725350830@qq.com>
2025-05-05 17:24:52 +0800

    complete bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100  # initial height in meters
+bounce_factor = 0.6  # bounce factor (fraction of height after bounce)
+
+for i in range(1, 11): 
+    height *= bounce_factor  # calculate new height after bounce
+    print(i, height)
