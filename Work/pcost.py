ee9c22cbcdeceee2a30ab4628798d234a1348880
lichen <1725350830@qq.com>
2025-05-06 23:50:43 +0800

    complete pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'r') as f:
+    _ = next(f)
+    count = 0
+    for line in f:
+        _, stock, price = line.split(',')
+        stock = int(stock.strip())
+        price = float(price.strip())
+        count += stock * price
+    print(f'Total cost {count:0.2f}')
+        
