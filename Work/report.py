f6d887776a5269cbc5e0c3b913df849f24af36e3
lichen <1725350830@qq.com>
2025-05-11 23:53:48 +0800

    complete report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,16 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    with open(filename, "r") as f:
+        _ = next(f)  # Skip the header
+        rows = csv.reader(f)
+        portfolio = []
+        for row in rows:
+            stock = int(row[1].strip())
+            price = float(row[2].strip())
+            portfolio.append((row[0], stock, price))
+
+    return portfolio
